865546962d0469570e1dcc4c3e99114b070c7850
vhh2752004 <148410630+vhh2752004@users.noreply.github.com>
2024-01-26 10:57:58 +0700

    DiemHocTap

---
+++ Model/Diemhoctap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; // Thêm dòng này
+using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLiSV.Models
+{
+    public class Diemhoctap
+    {
+        [Key]
+        public int masv { get; set; }
+    
+
+        [ForeignKey("Sinhvien")]
+        public int Sinhvien_Id { get; set; }
+
+        public virtual Sinhvien? Sinhvien { get; set; }
+
+        [ForeignKey("Monhoc")]
+        public int Monhoc_Id { get; set; }
+
+        public virtual Monhoc? Monhoc { get; set; }
+
+        public float? Diemthi { get; set; }
+
+        public float? Diemquatrinh { get; set; }
+    }
+}
