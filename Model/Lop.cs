355becada0eb277c0688a6c90d724e3ba736ef43
vhh2752004 <148410630+vhh2752004@users.noreply.github.com>
2024-01-26 11:01:21 +0700

    Lop

---
+++ Model/Lop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; // Thêm dòng này
+namespace QuanLiSV.Models
+{
+    public class Lop
+    {
+        [Key]
+        public int malop { get; set; }
+
+        [Required(ErrorMessage = "Tên lớp không được để trống")]
+        [StringLength(255, ErrorMessage = "Tên lớp không được quá 255 ký tự")]
+        public string tenlop { get; set; }
+    }
+}
