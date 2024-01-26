e1e96b99a7222c33ad873588192c031df7a338af
vhh2752004 <148410630+vhh2752004@users.noreply.github.com>
2024-01-26 10:39:49 +0700

    Mon Hoc

---
+++ Data/QuanLiSVContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuanLiSV.Models;
+
+namespace QuanLiSV.Data
+{
+    public class QuanLiSVContext : DbContext
+    {
+        public QuanLiSVContext (DbContextOptions<QuanLiSVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<QuanLiSV.Models.Diemhoctap> Diemhoctap { get; set; }
+
+        public DbSet<QuanLiSV.Models.Diemrenluyen> Diemrenluyen { get; set; }
+
+        public DbSet<QuanLiSV.Models.Ketquahoctap> Ketquahoctap { get; set; }
+
+        public DbSet<QuanLiSV.Models.Lichhoc> Lichhoc { get; set; }
+
+        public DbSet<QuanLiSV.Models.Lop> Lop { get; set; }
+
+        public DbSet<QuanLiSV.Models.Monhoc> Monhoc { get; set; }
+
+        public DbSet<QuanLiSV.Models.Sinhvien> Sinhvien { get; set; }
+
+        public DbSet<QuanLiSV.Models.User> User { get; set; }
+        public IEnumerable<object> StudentMarks { get; internal set; }
+    }
+}
